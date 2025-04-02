ed55196d8b117b21093b141a9645324f5320e658
Vaibhavp0696 <vaibhavprajapati386@gmail.com>
2025-04-02 18:19:08 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("Calculator")
+st.markdown("Welcome to my first web Calculator app!")
+
+c1, c2 = st.columns(2)
+
+fnum = c1.number_input("First number", value=0)
+snum = c2.number_input("Second number", value=0)
+
+options = ["Addition", "Subtraction", "Multiply", "Divide"]
+choice = st.radio("Select operation", options)
+
+button = st.button("Calulate")
+
+result = 0
+
+if button:
+    if choice == "Addition":
+        result = fnum + snum
+    if choice == "Subtraction":
+        result = fnum - snum
+    if choice == "Multiply":
+        result = fnum * snum
+    if choice == "Divide":
+        result = fnum / snum
+
+st.warning(f"Result: {result}")
